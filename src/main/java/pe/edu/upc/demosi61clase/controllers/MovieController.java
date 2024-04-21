560a85dffc4fdfa769c9c768b7ea4eab1185e50a
NiltonHuamanV <niltonhuaman16@gmail.com>
2024-04-21 11:58:21 -0500

    prueba 2

--- src/main/java/pe/edu/upc/demosi61clase/controllers/MovieController.java
+++ src/main/java/pe/edu/upc/demosi61clase/controllers/MovieController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 //prueba rama 1  peliculas controller
+//prueba para rama 2
 @RestController
 @RequestMapping("/peliculas")
 public class MovieController {
